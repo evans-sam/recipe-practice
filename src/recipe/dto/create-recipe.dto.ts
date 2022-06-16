e2678ec903e2ef7e0ea7e1b27ce5a073dc1689e4
Sam Evans <evans.sam.t@gmail.com>
2022-06-15 21:51:59 -0600

    adds type validation for recipes

--- src/recipe/dto/create-recipe.dto.ts
+++ src/recipe/dto/create-recipe.dto.ts
@@ -1,5 +1,15 @@
-export class CreateRecipeDto {
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { BaseRecipe } from '../../database/types';
+
+export class CreateRecipeDto implements BaseRecipe {
+  @IsNotEmpty()
   name: string;
+
+  @IsArray()
+  @IsString({ each: true })
   ingredients: string[];
+
+  @IsArray()
+  @IsString({ each: true })
   instructions: string[];
 }
